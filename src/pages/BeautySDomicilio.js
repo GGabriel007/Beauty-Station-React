2d681585dee6013f2458b352cbfe9201ffd6ad57
Gabriel <ggonza78@montgomerycollege.edu>
2024-07-22 19:40:13 -0400

    changed style
    Changed style

--- src/pages/BeautySDomicilio.js
+++ src/pages/BeautySDomicilio.js
@@ -9,36 +9,19 @@ const BeautySDomicilio = () => {
         <div className="top-information-form">
             <h2 className="header-information-form">SOLICITUD SERVICIO A DOMICILIO</h2>
         </div>
-        <div className="mid-information-form">
-            <form action = "https://formspree.io/f/mpwazzwv" method = "post">
-                <label for = "email">EMAIL:*</label>
-                <input type = "email" id ="email" name = "email" required/> <br/>
 
-                <label for ="name">NOMBRE Y APELLIDO:*</label>
-                <input type ="text" id ="name" name ="name" required/><br/>
+            {/* Embed Google Form */}
+            <iframe 
+            className='mid-information-form'
+              src="https://docs.google.com/forms/d/e/1FAIpQLScEDYWXQs7zn4kFG1GDCVsP9Xnq7dSpoKEP1fN1ikhiW2H5IQ/viewform?embedded=true" 
+              frameborder="0" 
+              marginheight="0" 
+              marginwidth="0"
+              title="Solicitud Servicio a Domicilio">
+              Loading…
+            </iframe>
 
-                <label for = "whatsapp">NUMERO DE WHATSAPP:*</label>
-                <input type = "tel" id ="whatsapp" name ="whatsapp" required/><br/>
-
-                <label for="date">FECHA DEL EVENTO:*</label>
-                <input type = "date" id="date" name="date" required/><br/>
-
-                <label for = "address">DIRECCIÓN O NOMBRE DE HOTEL DONDE DESEAN ARREGLARSE*: <div className ="second-Text">- ESPECIFICAR CIUDAD O ANTIGUA - TOMA EN CUENTA QUE LOS PRECIOS VARIAN SEGÚN UBICACIÓN</div></label>
-                <textarea id = "address" name = "address" required></textarea><br/>
-
-                <label for = "services">CANTIDAD DE SERVICOS QUE DESEEN COTIZAR:*<div className ="second-Text"> (MINIMO DE 6 MAQUILLAJES Y 6 PEINADOS PARA APLICAR A DOMICILIO)</div></label>
-                <input type ="text" id="services" name = "services" required/><br/>
-
-                <label for ="time">A QUE HORA NECESITAN ESTAR LISTAS?* <div className ="second-Text">(TOMA EN CONSIDERACIÓN SI SU FOTÓGRAFO LES HARÁ FOTOS)</div></label>
-                <input type = "time" id = "time" name = "time" required/><br/>
-
-                <label for ="notes">ALGO MÁS QUE CONSIDERA IMPORTANTE COMPARTIR CON NOSOTROS:*</label>
-                <textarea id = "notes" name = "notes" required></textarea>
-
-                <input type = "submit" value = "Submit"/>
-            </form>    
-        </div>
-    </div>
+      </div>
     </>
   );
 };
