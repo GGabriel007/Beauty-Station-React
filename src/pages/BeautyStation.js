d7bef5766a60da9aaccb8232a32afcdce5d29e4e
Gabriel <ggonza78@montgomerycollege.edu>
2024-07-25 14:10:07 -0400

    applying different changes
    trying different styles

--- src/pages/BeautyStation.js
+++ src/pages/BeautyStation.js
@@ -8,48 +8,56 @@ const BeautyStation = () => {
     <>
       <div className="information">
         <div className="top-information">
-          <h2 className="header-information">CURSOS</h2>
+          <h2 className="header-information">HOME</h2>
+        </div>
+        <div className='image-container-home'>
+        <div className = "top-imagen-home">
+        <img className = "background-imagen-home" src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_Aleh2.jpeg`} alt ="imagen of the salon"/>
+        <div className="overlay-home"></div>
+
         </div>
         <div className="mid-information">
           <div className="center">
-          <Link to = "/classes/classes-1">
+          <Link to = "/classes">
               <img className="icon" src={`${process.env.PUBLIC_URL}images/Class_1/imagen_Module_Hair.jpeg`} alt="Informacion de Cursos" />
           </Link>
-          <Link to = "/classes/classes-1" className='no-underline'>
-            <p className="title_links-classes">PEINADO</p>
+          <Link to = "/classes" className='no-underline'>
+            <p className="title_links-classes">CURSOS</p>
           </Link>
           </div>
           <div className="center">
-          <Link to = "/classes/classes-2" >
+          <Link to = "/servicio-a-domicilio" >
               <img className="icon" src={`${process.env.PUBLIC_URL}images/Class_1/imagen_Module_Mkup.jpeg`} alt="Informacion de Cursos" />
           </Link>
-          <Link to = "/classes/classes-2" className='no-underline'>
-            <p className="title_links-classes">MAQUILLAJE</p>
+          <Link to = "/servicio-a-domicilio" className='no-underline'>
+            <p className="title_links-classes">EVENTOS</p>
           </Link>
           </div>
         </div>
+        </div>
       </div>
       <div className="content">
-        <p className="first-text"><b>Encuentranos ubicados en la 20 Calle, zona 10, Cdad. de Guatemala, Guatemala</b></p>
+      <p className="first-text"><b>¡Contáctanos!</b></p>
+      
         <div className="waze">
-          <div className="location-text">¡También encuéntranos en Waze!</div>
-          <div className="block"></div>
-          <a href="https://www.waze.com/en/live-map/directions/beauty-station-nuevo-20-calle-la-plaza-20-cal-zona-10,-guatemala?place=w.176619666.1766262194.25981711">
-            <img className="waze-logo" src={`${process.env.PUBLIC_URL}/images/social_media/waze.svg`} alt="Waze Logo" />
-          </a>
+          <div className="location-text">¡Directamente al Whatsapp! <div className='block-home'></div><a className="single-icon-home" href="https://api.whatsapp.com/send?phone=50250177803&text=Encontr%C3%A9%20sus%20redes%20sociales%20y%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20de%20%3A" target="_blank" rel="noopener noreferrer">
+                <img className="icon-Social-media-home" src={`${process.env.PUBLIC_URL}/images/social_media/whatsapp.png`} alt="Whatsapp logo icons created by Pixel Perfect - Flaticon" />
+            </a></div>
+          </div>
+        
+        <p className="first-text"><b>Reseñas de Nuestros Clientes</b></p>
+        <div className="waze">
+          <div className="location-text">Estas son algunas de las opiniones y comentarios de quienes han confiado en nosotros para realzar su belleza.</div>
         </div>
-        <div className="map-container">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.371596291241!2d-90.49458159999999!3d14.5778894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a300389625b3%3A0x68e12c74df224866!2sBeauty%20Station!5e0!3m2!1sen!2sgt!4v1717607379678!5m2!1sen!2sgt"
-            width="600"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
+        
       </div>
+      <div className = "scrolling-wrapper-home">
+                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_Sala2.jpeg`} alt = "Gallery Image Loby"/>
+                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_Aleh2.jpeg`} alt = "Gallery Image Aleh 2"/>
+                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_reception.jpeg`} alt = "Gallery Image Reception"/>
+                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_Aleh.jpeg`} alt = "Gallery Image Aleh"/>
+                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_Sala.jpeg`} alt = "Gallery Image Loby"/>
+            </div>
     </>
   );
 };
