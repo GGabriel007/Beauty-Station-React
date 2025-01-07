a7f159811925c05205df5e9da1310ab1b1c7ef13
Gabriel <ggonza78@montgomerycollege.edu>
2025-01-07 14:07:31 -0500

    Image expantion fix
    When imagen was expand zoom in and zoom out was discard it.

--- src/pages/BeautySContacto.js
+++ src/pages/BeautySContacto.js
@@ -1,10 +1,32 @@
 // src/pages/BeautySContacto.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/beauty-SContacto.css';
 import { useLocation } from 'react-router-dom';
 
 
 const BeautySContacto = () => {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+    const images = [
+      `${process.env.PUBLIC_URL}/images/Beauty_Station_Sala2.jpeg`,
+      `${process.env.PUBLIC_URL}/images/Beauty_Station_Aleh2.jpeg`,
+      `${process.env.PUBLIC_URL}/images/Beauty_Station_reception.jpeg`,
+      `${process.env.PUBLIC_URL}/images/Beauty_Station_Aleh.jpeg`,
+      `${process.env.PUBLIC_URL}/images/Beauty_Station_Sala.jpeg`,
+    ];
+  
+  
+    const openModal = (src) => {
+      setSelectedImage(src);
+      setIsModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setIsModalOpen(false);
+      setSelectedImage(null);
+    };
 
   const location = useLocation();
     
@@ -33,13 +55,27 @@ const BeautySContacto = () => {
                 La mejor opción para cabello y maquillaje en Guatemala, con más de 11 años de experiencia en la industria de la belleza nupcial. Nuestro equipo ha sido capacitado con técnicas internacionales por nuestro MUA senior y peluquera Aleh, quien ha viajado por todo el mundo para aprender de los mejores expertos en Milán, Londres, Sao Paulo, Los Ángeles, Nueva York, Rusia y más, continuando siempre con la actualización de técnicas. Ofrecemos servicios presenciales para bodas de destino y eventos. Deja tu día especial en nuestras manos expertas.
             </div>
 
-            <div className = "scrolling-wrapper-contact">
-                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_Sala2.jpeg`} alt = "Gallery Image Loby"/>
-                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_Aleh2.jpeg`} alt = "Gallery Image Aleh 2"/>
-                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_reception.jpeg`} alt = "Gallery Image Reception"/>
-                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_Aleh.jpeg`} alt = "Gallery Image Aleh"/>
-                <img src ={`${process.env.PUBLIC_URL}/images/Beauty_Station_Sala.jpeg`} alt = "Gallery Image Loby"/>
-            </div>
+            {/* Scrolling Gallery */}
+      <div className="scrolling-wrapper-home">
+        {images.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            alt={`Gallery Image ${index + 1}`}
+            className="scrolling-image"
+            onClick={() => openModal(src)}
+          />
+        ))}
+      </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="modal" onClick={closeModal}>
+          <div className="modal-content">
+            <img className="modal-image" src={selectedImage} alt="Expanded view" />
+          </div>
+        </div>
+      )}
           </div> 
     </>
   );
