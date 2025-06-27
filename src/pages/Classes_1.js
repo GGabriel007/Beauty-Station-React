1330141582ef8d81bbefee893194ab38078603a3
Gabriel <ggonza78@montgomerycollege.edu>
2025-06-26 23:38:20 -0400

    uploading images for new page
    uploading new images for new pages and improve new buttons for link cards for hair style modules.

--- src/pages/Classes_1.js
+++ src/pages/Classes_1.js
@@ -26,16 +26,17 @@ const Classes1 = () => {
             <Link  to = "/classes/classes-1/Module_1Hair">
             <div className="center-class">
                 <Link className = 'imagen-link' to = "/classes/classes-1/Module_DayHair">
-                    <img className="icon-class" src={`${process.env.PUBLIC_URL}/images/Class_1/Module_1/imagen_module_Hair.jpeg`} alt="Informacion de Cursos"/>
+                    <img className="icon-class" src={`${process.env.PUBLIC_URL}/images/Class_1/Module_Day/imagen_module_Hair.jpeg`} alt="Informacion de Cursos"/>
                 </Link>
                 <div className="text-class">
                     
                         <p className = "class_links-class">Master en Waves Intensivo</p>
                     
                     <div className='description-class'>
-                        <div>1 DÍA </div>
+                        <div className='one-day'>1 DÍA </div>
                     <div className='class_dates_button'>
-                        <div>2 DE JULIO</div>
+                        <span className='default-text'>2 DE JULIO</span>
+                        <span className='hover-text'>MÁS INFORMACIÓN</span>
                     </div>
                     </div>
                 </div>
@@ -54,9 +55,11 @@ const Classes1 = () => {
                         <p className = "class_links-class">Master en Waves</p>
                     
                     <div className='description-class'>
-                    <div className='class_dates_button'><p>29 DE JULIO AL <br></br>
-                        19 DE AGOSTO
-                        </p></div>
+                    <div className='class_dates_button'>
+                        <span className='default-text'>29 DE JULIO AL <br></br>
+                        19 DE AGOSTO</span>
+                        <span className='hover-text'>MÁS INFORMACIÓN</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -73,9 +76,11 @@ const Classes1 = () => {
                         <p className = "class_links-class">Peinado para Eventos</p>
                         
                     <div className='description-class'>
-                    <div className='class_dates_button'><p>26 DE AGOSTO AL <br></br>
-                        30 DE SEPTIEMBRE
-                        </p></div>
+                    <div className='class_dates_button'>
+                        <span className='default-text'>26 DE AGOSTO AL <br></br>
+                        30 DE SEPTIEMBRE</span>
+                        <span className='hover-text'>MÁS INFORMACIÓN</span>
+                        </div>
                     </div>
                 </div>  
             </div>
@@ -92,9 +97,13 @@ const Classes1 = () => {
                         <p className = "class_links-class">Maestría en Novias y Tendencias</p>
                     
                     <div className='description-class'>
-                    <div className='class_dates_button'><p>7 DE OCTUBRE AL <br></br>
-                        30 DE OCTUBRE
-                        </p></div>
+                    <div className='class_dates_button'>
+
+                        <span className='default-text'>7 DE OCTUBRE AL <br></br>
+                        30 DE OCTUBRE</span>
+                        <span className='hover-text'>MÁS INFORMACIÓN</span>
+
+                        </div>
                     </div>
                 </div>  
             </div>
@@ -109,10 +118,13 @@ const Classes1 = () => {
                 <div className="text-class">
                     
                         <p className = "class_links-class">Curso Completo</p>
-                        <div className='description-class-complete'>
-                    <div className='class_dates_button'><p>29 DE JULIO AL <br></br>
-                        30 DE OCTUBRE
-                        </p></div>
+                        <div className='description-class'>
+                    <div className='class_dates_button'>
+                        <span className='default-text'>29 DE JULIO AL <br></br>
+                        30 DE OCTUBRE</span>
+                        <span className='hover-text'>MÁS INFORMACIÓN</span>
+                        
+                        </div>
                     </div>
                 </div>  
             </div>
