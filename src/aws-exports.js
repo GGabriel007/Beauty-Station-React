a6dd3fe06f4efc0e3d86b63b83dd6224fbc5bf4d
Gabriel <ggonza78@montgomerycollege.edu>
2026-03-25 10:34:17 -0400

    fix: sync generated aws-exports.js with live server

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "http://localhost:3000/,https://master.d121neu4gkwbak.amplifyapp.com/",
+        "redirectSignOut": "http://localhost:3000/,https://master.d121neu4gkwbak.amplifyapp.com/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
