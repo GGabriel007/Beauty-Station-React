e1b826fd2d63e15cb20090eb215ac04f196618ee
Gabriel <ggonza78@montgomerycollege.edu>
2026-03-26 01:36:29 -0400

    fix: rewrite Classes_1.js with class-card-link on all outer Links, add display:block safety rule

--- src/pages/Classes_1.js
+++ src/pages/Classes_1.js
@@ -22,18 +22,15 @@ const Classes1 = () => {
 
                 <div className="mid-information-module">
 
-                    <div className='space-class'></div>
-                    <Link to="/classes/course/master-waves-intensivo">
+                    <Link to="/classes/course/master-waves-intensivo" className="class-card-link">
                         <div className="center-class">
-                            <Link className='imagen-link' to="/classes/course/master-waves-intensivo">
+                            <div className='imagen-link'>
                                 <img className="icon-class" src={`${process.env.PUBLIC_URL}/images/Class_1/Module_Day/imagen_module_H.jpeg`} alt="Informacion de Cursos" />
-                            </Link>
+                            </div>
                             <div className="text-class">
-
                                 <p className="class_links-class">Master en Waves Intensivo</p>
-
                                 <div className='description-class'>
-                                    <div className='one-day'>1 DÍA </div>
+                                    <div className='one-day'>1 DÍA</div>
                                     <div className='class_dates_button'>
                                         <span className='default-text'>BAJO CITA</span>
                                         <span className='hover-text'>MÁS INFORMACIÓN</span>
@@ -42,94 +39,75 @@ const Classes1 = () => {
                             </div>
                         </div>
                     </Link>
-                    <div className='space-class'></div>
-                    <div className='space-class'></div>
 
-                    <Link to="/classes/course/master-waves">
+                    <Link to="/classes/course/master-waves" className="class-card-link">
                         <div className="center-class">
-                            <Link className='imagen-link' to="/classes/course/master-waves">
+                            <div className='imagen-link'>
                                 <img className="icon-class" src={`${process.env.PUBLIC_URL}/images/Class_1/Module_1/imagen_module_Hair.jpeg`} alt="Informacion de Cursos" />
-                            </Link>
+                            </div>
                             <div className="text-class">
-
                                 <p className="class_links-class">Master en Waves</p>
-
                                 <div className='description-class'>
                                     <div className='class_dates_button'>
-                                        <span className='default-text'>27 DE ENERO AL <br></br>
-                                            17 DE FEBRERO</span>
+                                        <span className='default-text'>27 DE ENERO AL <br/>17 DE FEBRERO</span>
                                         <span className='hover-text'>MÁS INFORMACIÓN</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </Link>
-                    <div className='space-class'></div>
-                    <div className='space-class'></div>
-                    <Link to="/classes/course/peinado-eventos">
+
+                    <Link to="/classes/course/peinado-eventos" className="class-card-link">
                         <div className="center-class">
-                            <Link className='imagen-link' to="/classes/course/peinado-eventos">
+                            <div className='imagen-link'>
                                 <img className="icon-class" src={`${process.env.PUBLIC_URL}/images/Class_1/Module_2/imagen_module_Hair.jpeg`} alt="Informacion de Cursos" />
-                            </Link>
+                            </div>
                             <div className="text-class">
-
                                 <p className="class_links-class">Peinado para Eventos</p>
-
                                 <div className='description-class'>
                                     <div className='class_dates_button'>
-                                        <span className='default-text'>24 DE FEBRERO AL <br></br>
-                                            7 DE ABRIL</span>
+                                        <span className='default-text'>24 DE FEBRERO AL <br/>7 DE ABRIL</span>
                                         <span className='hover-text'>MÁS INFORMACIÓN</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </Link>
-                    <div className='space-class'></div>
-                    <div className='space-class'></div>
-                    <Link to="/classes/course/maestria-novias">
+
+                    <Link to="/classes/course/maestria-novias" className="class-card-link">
                         <div className="center-class">
-                            <Link className='imagen-link' to="/classes/course/maestria-novias">
+                            <div className='imagen-link'>
                                 <img className="icon-class" src={`${process.env.PUBLIC_URL}/images/Class_1/Module_3/imagen_module_Hair.jpeg`} alt="Informacion de Cursos" />
-                            </Link>
+                            </div>
                             <div className="text-class">
-
                                 <p className="class_links-class">Maestría en Novias y Tendencias</p>
-
                                 <div className='description-class'>
                                     <div className='class_dates_button'>
-
-                                        <span className='default-text'>15 DE ABRIL AL <br></br>
-                                            7 DE MAYO</span>
+                                        <span className='default-text'>15 DE ABRIL AL <br/>7 DE MAYO</span>
                                         <span className='hover-text'>MÁS INFORMACIÓN</span>
-
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </Link>
-                    <div className='space-class'></div>
-                    <div className='space-class'></div>
-                    <Link to="/classes/course/curso-completo-peinado">
-                        <div className="center-class">
-                            <Link className='imagen-link' to="/classes/course/curso-completo-peinado">
-                                <img className="icon-class" src={`${process.env.PUBLIC_URL}/images/Class_1/Module_4/imagen_module_Hair.jpeg`} alt="Informacion de Cursos" />
-                            </Link>
-                            <div className="text-class">
 
+                    <Link to="/classes/course/curso-completo-peinado" className="class-card-link">
+                        <div className="center-class">
+                            <div className='imagen-link'>
+                                <img className="icon-class" src={`${process.env.PUBLIC_URL}/images/Class_1/Module_4/imagen_module_Hair.jpeg`} alt="Informacion de Cursos" />
+                            </div>
+                            <div className="text-class">
                                 <p className="class_links-class">Curso Completo</p>
                                 <div className='description-class'>
                                     <div className='class_dates_button'>
-                                        <span className='default-text'>27 DE ENERO AL <br></br>
-                                            7 DE MAYO</span>
+                                        <span className='default-text'>27 DE ENERO AL <br/>7 DE MAYO</span>
                                         <span className='hover-text'>MÁS INFORMACIÓN</span>
-
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </Link>
-                    <div className='space-class'></div>
+
                 </div>
             </div>
         </>
