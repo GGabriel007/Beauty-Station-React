4ea88e1e0ad9965c16d63681b9758eb0e5f6aa94
Gabriel <ggonza78@montgomerycollege.edu>
2026-03-25 20:41:53 -0400

    fix: restructure Cursos page cards JSX to match homepage glass-card layout for edge-to-edge image fill

--- src/pages/BeautySClasses.js
+++ src/pages/BeautySClasses.js
@@ -27,28 +27,18 @@ const BeautySClasses = () => {
         </div>
 
         <div className="mid-information-classes">
-        <div className="center-classes">
-                <Link to = "/classes/classes-2">
-                    <img className="icon-classes" src={`${process.env.PUBLIC_URL}/images/Class_1/imagen_Module_Mkup.jpeg`} alt="Informacion de Cursos"/>
-                </Link>
-                <div className="text-classes">
-                        <Link to = "/classes/classes-2">
-                        <p className = "title_links-cursos">MAQUILLAJE</p>
-                        </Link>
-                        
+          <Link to="/classes/classes-2" className="center-classes">
+            <div className="glass-card-image-wrap">
+              <img className="icon-classes" src={`${process.env.PUBLIC_URL}/images/Class_1/imagen_Module_Mkup.jpeg`} alt="Informacion de Cursos"/>
             </div>
-        </div>
-            <div className="center-classes">
-                <Link to = "/classes/classes-1">
-                    <img className="icon-classes" src={`${process.env.PUBLIC_URL}/images/Class_1/imagen_Module_Hair.jpeg`} alt="Informacion de Cursos"/>
-                </Link>
-                <div className="text-classes">
-                    <Link to = "/classes/classes-1">
-                        <p className = "title_links-cursos">PEINADO</p>
-                    </Link>
-                    
-                </div>
+            <div className="title_links-cursos">MAQUILLAJE</div>
+          </Link>
+          <Link to="/classes/classes-1" className="center-classes">
+            <div className="glass-card-image-wrap">
+              <img className="icon-classes" src={`${process.env.PUBLIC_URL}/images/Class_1/imagen_Module_Hair.jpeg`} alt="Informacion de Cursos"/>
             </div>
+            <div className="title_links-cursos">PEINADO</div>
+          </Link>
         </div>
     </div>
     </div>
